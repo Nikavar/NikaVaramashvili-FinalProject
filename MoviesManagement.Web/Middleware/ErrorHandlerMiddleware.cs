749bcb17a84648da11c052bc547967ec7a822ca4
Nikavar <45462426+Nikavar@users.noreply.github.com>
2022-04-14 15:44:03 +0400

    Add files via upload

---
+++ MoviesManagement.Web/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using MovieWebApi.Web;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApi.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlerMiddleware> _logger;
+
+        public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+        public async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+
+            var exception = new ApiError(context, ex);
+            _logger.LogError(exception.TraceId, exception.LogLevel, ex, exception.Type);
+
+            var result = JsonConvert.SerializeObject(exception);
+            context.Response.Clear();
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = exception.Status.Value;
+
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ MoviesManagement.Web/Middleware/RequestResponseMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApi.Web.Middleware
+{
+    public class RequestResponseMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestResponseMiddleware> _logger;
+
+        public RequestResponseMiddleware(RequestDelegate next, ILogger<RequestResponseMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var request = context.Request.Path;
+            _logger.LogInformation($"Request Details: {request}");
+
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+
+            var response = context.Response.StatusCode;
+            _logger.LogInformation($"Response Details: {response}");
+        }
+    }
+}
