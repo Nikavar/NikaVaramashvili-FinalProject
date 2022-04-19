ab9471f80f7335d9aa678f235469bdcb4a77514b
Nikavar <45462426+Nikavar@users.noreply.github.com>
2022-04-19 12:32:20 +0400

    Add files via upload

---
+++ MovieWebApi/Mappings/MapsterConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+using MoviesManagement.Service.Models;
+using MovieWebApi.Domain.POCO;
+using MovieWebApi.Service.Models;
+using MovieWebApi.Service.Models.JWT;
+using MovieWebApi.Web.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApi.Web.Mappings
+{
+    public static class MapsterConfiguration
+    {
+        public static void RegisterMaps(this IServiceCollection service)
+        {
+            TypeAdapterConfig<Users, UserRegisterServiceModel>
+                .NewConfig()
+                .TwoWays();
+
+            // Service <==> POCO
+            TypeAdapterConfig<MovieServiceModel, Movies>
+            .NewConfig()
+            .TwoWays();
+
+            // Service <==> DTO
+            TypeAdapterConfig<MovieServiceModel, MovieDTO>
+            .NewConfig()
+            .TwoWays();
+        }
+    }
+}
