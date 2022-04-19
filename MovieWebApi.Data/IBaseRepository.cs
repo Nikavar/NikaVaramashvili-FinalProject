f1ef488eabd4337316660db56d65bfe75f8db2ef
Nikavar <45462426+Nikavar@users.noreply.github.com>
2022-04-19 12:39:44 +0400

    Add files via upload

---
+++ MovieWebApi.Data/IBaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWebApi.Data
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        #region Methods
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllAsync(params Expression<Func<T, object>>[] property);
+        Task<T> GetAsync(params object[] key);
+        Task AddAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveAsync(params object[] key);
+        Task UpdateAsync(T entity);
+        Task<bool> AnyAsync(Expression<Func<T,bool>> predicate);
+        #endregion
+
+        #region Sets
+        IQueryable<T> Table { get; }
+
+        #endregion
+    }
+}
---
+++ MovieWebApi.Data/IMovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using MovieWebApi.Domain.POCO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWebApi.Data
+{
+    public interface IMovieRepository
+    {
+        Task<List<Movies>> GetAllAsync();
+        Task<Movies> GetByIdAsync(int id);
+        Task<int> CreateAsync(Movies movie);        
+        Task UpdateAsync(Movies movie);        
+        Task DeleteAsync(int id);
+        Task<bool> Exists(int id);
+        Task PublishAsync(int id);
+        Task RejectAsync(int id);
+    }
+}
