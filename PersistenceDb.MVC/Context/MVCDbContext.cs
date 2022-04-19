e10756b4f94784723c1f57b0a454aaaf697b23af
Nikavar <45462426+Nikavar@users.noreply.github.com>
2022-04-19 12:48:10 +0400

    Add files via upload

---
+++ PersistenceDb.MVC/Context/MVCDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MovieWebApi.Domain.POCO;
+using MovieWebApi.PersistenceDB.Context;
+
+namespace PersistenceDb.MVC.Context
+{
+    public class MVCDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public MVCDbContext(DbContextOptions<MVCDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MVCDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+
+        }
+        public DbSet<Movies> Movies { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderMovie> OrderMovies { get; set; }
+    }
+
+}
