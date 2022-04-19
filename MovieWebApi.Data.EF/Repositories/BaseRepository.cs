c17395225fa421d980c337b0fd5dbe05a9820061
Nikavar <45462426+Nikavar@users.noreply.github.com>
2022-04-19 12:43:13 +0400

    Add files via upload

---
+++ MovieWebApi.Data.EF/Repositories/BaseRepository.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieWebApi.PersistenceDB.Context;
+using PersistenceDb.MVC.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MovieWebApi.Data.EF
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+
+        #region Protected
+        protected readonly MVCDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+        #endregion
+
+        #region Ctor
+        public BaseRepository(MVCDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        #endregion
+
+        #region Property
+        public IQueryable<T> Table
+        {
+            get { return _dbSet; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(params object[] key)
+        {
+            return await _dbSet.FindAsync(key);
+        }
+
+        public async Task<T> GetAsync(string expresion, params object[] key)
+        {
+            return await _dbSet.FindAsync(key);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            if (entity == null)
+                return;
+
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+        public async Task RemoveAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(params object[] key)
+        {
+            var entity = await GetAsync(key);
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.AnyAsync(predicate);
+        }
+
+        public async Task<List<T>> GetAllAsync(params Expression<Func<T, object>>[] property)
+        {
+            IQueryable<T> query = _dbSet;
+            for (int i = 0; i < property.Length; i++)
+            {
+                query = property.Aggregate(query, (x, property) => x.Include(property));
+            }
+
+            return await query.ToListAsync();
+        }
+
+        #endregion
+
+    }
+}
---
+++ MovieWebApi.Data.EF/Repositories/MovieRepository.cs
@@ -0,0 +1,63 @@
+﻿using MovieWebApi.Domain.POCO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieWebApi.Data.EF
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly IBaseRepository<Movies> _baseRepository;
+
+        public MovieRepository(IBaseRepository<Movies> repository)
+        {
+            _baseRepository = repository;
+        }
+
+        public async Task<List<Movies>> GetAllAsync()
+        {
+            return await _baseRepository.GetAllAsync();
+        }
+
+        public async Task<Movies> GetByIdAsync(int id)
+        {
+            return await _baseRepository.GetAsync(id);
+        }
+
+        public async Task<int> CreateAsync(Movies movie)
+        {
+            await _baseRepository.AddAsync(movie);
+            return movie.Id;
+        }
+
+        public async Task UpdateAsync(Movies movie)
+        {
+            await _baseRepository.UpdateAsync(movie);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _baseRepository.RemoveAsync(id);
+        }
+
+        public async Task PublishAsync(int id)
+        {
+            var publishedMovie = await _baseRepository.GetAsync(id);
+            publishedMovie.IsActive = true;
+            await _baseRepository.UpdateAsync(publishedMovie);
+        }
+
+        public async Task RejectAsync(int id)
+        {
+            var rejectedMovie = await _baseRepository.GetAsync(id);
+            rejectedMovie.IsActive = false;
+            await _baseRepository.UpdateAsync(rejectedMovie);
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            return await _baseRepository.AnyAsync(x => x.Id == id);
+        }
+    }
+}
