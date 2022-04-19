6a845eeaedec65a565ea0a8bbac14624aaae6623
Nikavar <45462426+Nikavar@users.noreply.github.com>
2022-04-19 12:34:34 +0400

    Add files via upload

---
+++ MovieWebApi/SwaggerConfigs/SwaggerConfig.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWebApi.Web.SwaggerConfigs
+{
+	public class SwaggerConfig : IConfigureOptions<SwaggerGenOptions>
+	{
+		private readonly IApiVersionDescriptionProvider _provider;
+
+		public SwaggerConfig(IApiVersionDescriptionProvider provider)
+		{
+			_provider = provider;
+		}
+
+		public void Configure(SwaggerGenOptions options)
+		{
+
+			foreach (var description in _provider.ApiVersionDescriptions)
+			{
+				options.SwaggerDoc($"{description.GroupName}", new OpenApiInfo
+				{
+					Version = description.ApiVersion.ToString(),
+					Title = "Movies.ItAcademy.Ge API",
+					Description = "Movie Management Project_ ASP.NET Core Web API",
+					Contact = new OpenApiContact
+					{
+						Name = "Nikoloz Varamashvili",
+						Email = "varnikoloz@gmail.com"
+					}
+				}); ;
+			}
+		}
+
+		private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+		{
+			var info = new OpenApiInfo()
+			{
+				Title = "Movies Management Web API",
+				Version = description.ApiVersion.ToString(),
+			};
+
+			return info;
+		}
+	}
+}
---
+++ MovieWebApi/SwaggerConfigs/SwaggerDefaultValues.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace MovieWebApi.Web.SwaggerConfigs
+{
+    public class SwaggerDefaultValues : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+			var apiDescription = context.ApiDescription;
+			operation.Deprecated |= apiDescription.IsDeprecated();
+
+			if (operation.Parameters == null)
+				return;
+
+			foreach (var param in operation.Parameters)
+			{
+				var description = apiDescription.ParameterDescriptions.First(p => p.Name == param.Name);
+				if (param.Description == null)
+				{
+					param.Description = description.ModelMetadata?.Description;
+				}
+
+				if (param.Schema.Default == null && description.DefaultValue != null)
+				{
+					param.Schema.Default = new OpenApiString(description.DefaultValue.ToString());
+				}
+
+				param.Required |= description.IsRequired;
+			}
+		}
+    }
+}
